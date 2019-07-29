25843fe7c0dd947191db1090a1b039189a742f13
freestyle09 <jozefrzadkosz09@gmail.com>
2019-07-29 14:56:06 +0200

    test

---
+++ src/shared/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <div className='App-header'>
+          <h2>Welcome to React in the Server</h2>
+          <h3>Siema</h3>
+          <h3>Siema</h3>
+        </div>
+        <p className='App-intro'>Isn't this cool?</p>
+      </div>
+    );
+  }
+}
+export default App;
